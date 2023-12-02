8ca9bec601bd5fa384b3807e65f071f9274250b8
Daan Gunnink <daan@kreukelz.one>
2023-12-02 21:16:08 +0100

    Day 1: Trebuchet?!

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,53 @@
+import java.lang.Exception
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    Day01()
+}
+
+class Day01 : AdventOfCode(testPart1 = null, testPart2 = 281) {
+    override fun part1(input: List<String>): Any {
+        return input.splitAfter("").map {
+            it.map {
+                it.filter { it.isDigit() }.let {
+                    "${it.first()}${it.last()}".toInt()
+                }
+            }.sum()
+        }.first()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    override fun part2(input: List<String>): Any {
+        return input.splitAfter("").map {
+            it.map { line ->
+                val result = parseWordsFromList(line)
+                val filtered = result.filter { char -> char.isDigit() }.let { number ->
+                    "${number.first()}${number.last()}".toInt()
+                }
+                filtered
+            }.sum()
+        }.first()
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    private fun parseWordsFromList(line: String): String {
+        var newLine = line
+        val numberWords = mapOf(
+                "one" to "1",
+                "two" to "2",
+                "three" to "3",
+                "four" to "4",
+                "five" to "5",
+                "six" to "6",
+                "seven" to "7",
+                "eight" to "8",
+                "nine" to "9"
+        )
 
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+        newLine.findAnyOf(numberWords.keys, 0, true)?.let {
+            newLine = newLine.replaceRange(it.first, it.first, numberWords[it.second]!!)
+        }
+        newLine.findLastAnyOf(numberWords.keys, newLine.lastIndex, true)?.let {
+            newLine = newLine.replace(it.second, numberWords[it.second]!!)
+        }
+
+        return newLine
+    }
 }
